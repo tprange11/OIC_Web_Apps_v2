30734f5af01d14f45f4877ee21a2c8b67ecfa1c1
Brian Christensen <brianc@wi.rr.com>
2020-07-15 10:25:25 -0500

    add figure skating group context

--- figure_skating/contexts.py
+++ figure_skating/contexts.py
@@ -3,3 +3,7 @@ from django.urls import resolve
 # This is required to reference the appname in Django templates
 def appname(request):
     return { 'appname': resolve(request.path).app_name }
+
+def in_group_figure_skating(request):
+    in_group = request.user.groups.filter(name='Figure Skating').exists()
+    return { 'in_group_figure_skating': in_group}
