630145fea213959af02faa43af3d48f306c5f87c
Brian Christensen <brianc@wi.rr.com>
2021-01-04 08:44:55 -0600

    add user.is_active to scrape emails

--- womens_hockey/scrape_womens_hockey_dates.py
+++ womens_hockey/scrape_womens_hockey_dates.py
@@ -105,35 +105,36 @@ def send_skate_dates_email():
         womens_hockey_email=True).select_related('user')
 
     for recipient in recipients:
-        to_email = [recipient.user.email]
-        from_email = 'no-reply@mg.oicwebapps.com'
-        subject = 'New Womens Hockey Skate Date Added'
+        if recipient.user.is_active:
+            to_email = [recipient.user.email]
+            from_email = 'no-reply@mg.oicwebapps.com'
+            subject = 'New Womens Hockey Skate Date Added'
 
-        # Build the plain text message
-        text_message = f'Hi {recipient.user.first_name},\n\n'
-        text_message += f'New Womens Hockey skate dates are now available online. Sign up at the url below.\n\n'
-        text_message += f'https://www.oicwebapps.com/web_apps/womens_hockey/\n\n'
-        text_message += f'If you no longer wish to receive these emails, log in to your account,\n'
-        text_message += f'click on your username and change the email settings in your profile.\n\n'
-        text_message += f'Thank you for using OICWebApps.com!\n\n'
+            # Build the plain text message
+            text_message = f'Hi {recipient.user.first_name},\n\n'
+            text_message += f'New Womens Hockey skate dates are now available online. Sign up at the url below.\n\n'
+            text_message += f'https://www.oicwebapps.com/web_apps/womens_hockey/\n\n'
+            text_message += f'If you no longer wish to receive these emails, log in to your account,\n'
+            text_message += f'click on your username and change the email settings in your profile.\n\n'
+            text_message += f'Thank you for using OICWebApps.com!\n\n'
 
-        # Build the html message
-        html_message = render_to_string(
-            'womens_hockey_skate_dates_email.html',
-            {
-                'recipient_name': recipient.user.first_name,
-            }
-        )
-
-        # Send email to each recipient separately
-        try:
-            mail = EmailMultiAlternatives(
-                subject, text_message, from_email, to_email
+            # Build the html message
+            html_message = render_to_string(
+                'womens_hockey_skate_dates_email.html',
+                {
+                    'recipient_name': recipient.user.first_name,
+                }
             )
-            mail.attach_alternative(html_message, 'text/html')
-            mail.send()
-        except:
-            return
+
+            # Send email to each recipient separately
+            try:
+                mail = EmailMultiAlternatives(
+                    subject, text_message, from_email, to_email
+                )
+                mail.attach_alternative(html_message, 'text/html')
+                mail.send()
+            except:
+                return
 
 
 if __name__ == "__main__":
