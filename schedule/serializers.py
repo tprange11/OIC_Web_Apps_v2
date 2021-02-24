d8f414ac5da1088ba9216ada027024c6cf75d416
Brian Christensen <brianc@wi.rr.com>
2021-02-24 12:26:23 -0600

    add countdown_time

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from .models import RinkSchedule
 
-# from datetime import datetime, date
+from datetime import datetime, date
 
 
 class RinkScheduleSerializer(serializers.ModelSerializer):
 
     start_time = serializers.TimeField(format='%I:%M %p')
     end_time = serializers.TimeField(format='%I:%M %p')
+    countdown_time = serializers.SerializerMethodField()
 
     class Meta:
         model = RinkSchedule
-        fields = ['id', 'schedule_date', 'start_time', 'end_time', 'rink', 'event', 'home_locker_room', 'visitor_locker_room']
+        fields = ['id', 'schedule_date', 'start_time', 'end_time', 'countdown_time', 'rink', 'event', 'home_locker_room', 'visitor_locker_room']
+
+    def get_countdown_time(self, obj):
+        return date.isoformat(datetime.now())+" "+obj.end_time.strftime('%H:%M:%S')
