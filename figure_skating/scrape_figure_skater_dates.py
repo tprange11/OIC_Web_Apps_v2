7c0872cffb4cecd51aa4c624fe88f9bb018fa6d7
Brian Christensen <brianc@wi.rr.com>
2021-03-10 17:38:56 -0600

    scrape figure skate dates

--- figure_skating/scrape_figure_skater_dates.py
+++ figure_skating/scrape_figure_skater_dates.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
 
     # Every Friday scrape the next 12 days for Figure Skating dates
     # if the_date.weekday() == 4:
-    for x in range(26):
+    for x in range(22):
         if the_date.weekday() == 4 or the_date.weekday() == 5:
             # print(the_date)
             scrape_date = date.isoformat(the_date)
