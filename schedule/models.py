96cd4f00096a05bff02caa9fd08da1284dfabf10
Brian Christensen <brianc@wi.rr.com>
2021-03-18 10:16:50 -0500

    add help text

--- schedule/models.py
+++ schedule/models.py
@@ -8,7 +8,7 @@ class RinkSchedule(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
     rink = models.CharField(max_length=15)
-    event = models.CharField(max_length=50)
+    event = models.CharField(max_length=50, help_text="Field format for two teams: Put home team first and separate with \' vs \', a space then, lowercase vs, then a space")
     home_locker_room = models.CharField(max_length=50, null=True, blank=True)
     visitor_locker_room = models.CharField(max_length=50, null=True, blank=True)
     notes = models.CharField(max_length=50, blank=True)
@@ -16,6 +16,8 @@ class RinkSchedule(models.Model):
     class meta:
         ordering = ['end_time']
         unique_together = [['schedule_date', 'start_time', 'end_time', 'rink']]
+        verbose_name_plural = 'Rink Schedule'
 
     def __str__(self):
         return f"{self.schedule_date} Start: {self.start_time}, End: {self.end_time}, Rink: {self.rink}, Event: {self.event}, Notes: {self.notes}"
+        
