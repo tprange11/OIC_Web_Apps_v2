52ea9c19c544385187a4ed40ea35315bb1fcab80
Brian Christensen <brianc@wi.rr.com>
2022-06-08 09:45:09 -0500

    hide old skates

--- accounts/forms.py
+++ accounts/forms.py
@@ -27,21 +27,21 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['user', 'stick_and_puck_email', 'figure_skating_email',
-                 'thane_storck_email', 'adult_skills_email', 'mike_schultz_email', 'yeti_skate_email', 
-                 'womens_hockey_email', 'bald_eagles_email', 'lady_hawks_email', 'chs_alumni_email',
+                 'adult_skills_email', 'yeti_skate_email', 
+                 'womens_hockey_email', 'bald_eagles_email', 'lady_hawks_email',
                  'kranich_email', 'nacho_skate_email', 'ament_email']
         widgets = {'user': forms.HiddenInput()}
         labels = {
             'stick_and_puck_email': 'Receive Stick and Puck emails.',
             'figure_skating_email': 'Receive Figure Skating emails.',
-            'thane_storck_email': 'Receive Thane Storck Skate emails.',
-            'adult_skills_email': 'Receive Adult Skills Skate emails.',
-            'mike_schultz_email': 'Receive Mike Schultz Skate emails.',
+            # 'thane_storck_email': 'Receive Thane Storck Skate emails.',
+            'adult_skills_email': 'Receive Adult Skills emails.',
+            # 'mike_schultz_email': 'Receive Mike Schultz Skate emails.',
             'yeti_skate_email': 'Receive Yeti Skate emails.',
             'womens_hockey_email': 'Receive Womens Hockey Emails.',
             'bald_eagles_email': 'Receive Bald Eagles Skate Emails.',
             'lady_hawks_email': 'Receive Lady Hawks Skate Emails.',
-            'chs_alumni_email': 'Receive CHS Alumni Skate Emails.',
+            # 'chs_alumni_email': 'Receive CHS Alumni Skate Emails.',
             'kranich_email': 'Receive Kranich Skate Emails.',
             'nacho_skate_email': 'Receive Nacho Skate Emails.',
             'ament_email': 'Receive Ament Skate Emails.',
