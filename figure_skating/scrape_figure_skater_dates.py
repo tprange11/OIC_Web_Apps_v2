7180e4ae5a825ff7093f3705159a8bfc93d1e24e
Brian Christensen <brianc@wi.rr.com>
2023-10-30 15:54:44 -0500

    Nov Dates

--- figure_skating/scrape_figure_skater_dates.py
+++ figure_skating/scrape_figure_skater_dates.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     # the_date = date.today()
     send_email = False
 
-    get_schedule_data("10/01/2023", "10/25/2023")
+    get_schedule_data("11/01/2023", "11/25/2023")
 
     if len(skate_dates) != 0:
         # send_email = add_skate_dates(skate_dates)
