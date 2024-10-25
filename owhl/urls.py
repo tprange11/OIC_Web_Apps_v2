e7c63b83add6ffe9df023648aba1de2fb853a5d8
Just Another Miner <tprange@gmail.com>
2024-10-25 17:04:43 -0500

    Update urls.py
    Add remove button

--- owhl/urls.py
+++ owhl/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.OWHLSkateDateListView.as_view(), name='owhl'),
     path('register/<pk>/', views.CreateOWHLSkateSessionView.as_view(), name='register'),
     path('register/', views.CreateOWHLSkateSessionView.as_view(), name='register'),
-    path('session/remove/<pk>', views.DeleteOWHLSkateSessionView.as_view(), name='session-remove'),
+    path('session/remove/<pk>/<paid>/<skater_pk>', views.DeleteOWHLSkateSessionView.as_view(), name='session-remove'),
     path('session/list/', views.OWHLSkateDateStaffListView.as_view(), name='owhl-sessions'),
 ]
