5c14e0df8e6133630b659aa96372f8230d566555
Just Another Miner <tprange@gmail.com>
2024-11-03 14:38:17 -0600

    Update views.py
    fixed the redirect url.

--- nacho_skate/views.py
+++ nacho_skate/views.py
@@ -229,7 +229,7 @@ class DeleteNachoSkateSessionView(LoginRequiredMixin, DeleteView):
         try:
             send_mail(subject, success_msg, from_email, recipients)
             messages.add_message(self.request, messages.INFO, 'Email message has been sent to the skater!')
-            self.success_url = reverse_lazy('owhl:owhl')
+            self.success_url = reverse_lazy('nacho_skate:index')
         #except:
             #messages.add_message(self.request, messages.ERROR, 'Oops, something went wrong!  Please try again.')
             #return reverse('contact:contact-form', kwargs={ 'form': form.cleaned_data })
